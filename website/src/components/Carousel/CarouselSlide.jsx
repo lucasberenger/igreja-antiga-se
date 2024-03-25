0a2f86d496a6be7c748506d1fcd57ee3875d4a58
Thiago <thberenger27@gmail.com>
2024-03-25 18:48:26 -0300

    delete folder Carousel Images

--- website/src/components/Carousel/CarouselSlide.jsx
+++ website/src/components/Carousel/CarouselSlide.jsx
@@ -1,19 +1,15 @@
-import Carousel from 'react-bootstrap/Carousel';
-import CarouselImages from './CarouselImages';
-
 import './Carousel.modules.css'
 
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-
 const CarouselSlide = () => {
 
     const data = [
-        {id:'1', image: '../../assets/missa.jpg'},
-        {id:'2', image: '../../assets/santissimo.jpg'},
-        {id:'3', image: '../../assets/topo2.jpg'},
-        {id:'4', image: '../../assets/topoAltar.jpg'}
-    ]
+     { id: '1', image: '../../assets/missa.jpg' },
+     { id: '2', image: '../../assets/santissimo.jpg'},
+     { id: '3', image: '../../assets/topo2.jpg' },
+     { id: '4', image: '../../assets/topoAltar.jpg'}
+]
 
     return (
         <>
