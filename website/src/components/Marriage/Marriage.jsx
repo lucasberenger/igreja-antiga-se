3668ca7c387bb4cd76e689732f6fe18cf1aeb048
Thiago <thberenger27@gmail.com>
2024-03-27 16:41:52 -0300

    Marriage responsive
     - Marriage responsive
     - responsive slides

--- website/src/components/Marriage/Marriage.jsx
+++ website/src/components/Marriage/Marriage.jsx
@@ -4,46 +4,72 @@ import './Marriage.modules.css'
 import Button from 'react-bootstrap/Button';
 
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { useState, useEffect } from 'react'
 
+// pictures
 import casamentoFoto from '../../assets/casamentoFoto.jpg'
 import casamentoReal from '../../assets/casamentoReal.jpg'
 import casamentoFoto2 from '../../assets/casamentoFoto2.jpg'
 import casamentoFoto4 from '../../assets/casamentoFoto4.jpg'
 import casamentoFoto5 from '../../assets/casamentoFoto5.jpg'
 import casamentoFlores from '../../assets/casamentoFlores.jpg'
+// -----------
 
-const images = [
-  { id: '1', image: casamentoFoto},
-  { id: '2', image: casamentoReal},
-  { id: '3', image: casamentoFoto2},
-  { id: '4', image: casamentoFoto4},
-  { id: '5', image: casamentoFoto5},
-  { id: '6', image: casamentoFlores},
-]
 
 const Marriage = () => {
+
+  const [slidePerView, setSlidePerView] = useState(2)
+
+  const images = [
+    { id: '1', image: casamentoFoto},
+    { id: '2', image: casamentoReal},
+    { id: '3', image: casamentoFoto2},
+    { id: '4', image: casamentoFoto4},
+    { id: '5', image: casamentoFoto5},
+    { id: '6', image: casamentoFlores},
+  ]
+
+  useEffect(() => {
+    function handleResize() {
+      if(window.innerWidth < 900) {
+        setSlidePerView(2)
+      } else {
+        setSlidePerView(3)
+      }
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+
+    return() => {
+      window.removeEventListener('resize', handleResize)
+    }
+
+  }, [])
+
+  
   return (
     <div className='marriage-container'>
 
       <div className="marriage-container_text">
 
         <div className="marriage-container_title">
-          <h1>Agenda de Casamentos está aberta! </h1>
+          <h1>Agenda de Casamentos aberta! </h1>
           <p>"Faça como a Princesa Isabel: escolha a Antiga Sé para o seu casamento real. Agende sua data agora mesmo!"</p>
         </div>
 
         <div className="marriage-container_button">
         <Button variant="outline-primary" className='marriage_button'>Agende o seu!</Button>
         </div>
-        
+
       </div>
     
       <div className="marriage-container_slides">
       
       <Swiper 
-      slidesPerView={3}
+      slidesPerView={slidePerView}
       pagination= {{ clickable: true }}
-      navigation
       autoplay={{ delay: 4000 }}
       className='marriage-slider'
       >
