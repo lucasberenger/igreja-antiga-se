9b72f2d4df85b36f1e6cc1a43819a6afb1e77bd6
Thiago <thberenger27@gmail.com>
2024-04-05 11:41:26 -0300

    History div Home page
     - calc history year
     - responsive

--- website/src/components/Historia/History.jsx
+++ website/src/components/Historia/History.jsx
@@ -9,10 +9,23 @@ import  { Link } from 'react-router-dom'
 import coroacaoDomPedro from '../../assets/coroacaoDomPedro.jpg'
 
 const History = () => {
-  return (
+
+  const currentYear = new Date().getFullYear()
+  const result = currentYear - 1590
+
+  console.log(result)
+
+
+  return (    
     <>
         <div className="history_container">
             <div className="history_text">
+
+                <div className="history_year">
+                  <h2>{result}</h2>
+                  <h3>Anos de História</h3>
+                </div>
+
                 <h4>A história da Igreja de Nossa Senhora do Carmo da Antiga Sé se confunde com a história do Brasil.</h4>
 
                 <p>A Antiga Sé foi o palco de alguns dos mais importantes momentos dessa história, como a coroação de Dom Pedro I e Dom Pedro II, além de cenário dos casamentos reais.</p>
